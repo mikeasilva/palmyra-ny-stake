ac5e58b70b84e67d1c23cd6e2ca7cd3632e1c50c
Mike Silva <mike.a.silva@gmail.com>
2024-11-03 20:52:49 -0500

    Updates for template name changes

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
     # print(str(request.url))
-    # program_template = "2024_sunday_morning.html"
-    program_template = "redirect.html"
-    #program_template = "2024_saturday_night.html"
-    #program_template = "2024_leadership.html"
+    program_template = "sunday_morning.html"
+    #program_template = "redirect.html"
+    #program_template = "saturday_night.html"
+    #program_template = "leadership.html"
     return templates.TemplateResponse(program_template, {"request": request})
 
 """
 @app.get("/conference/program/{meeting}", response_class=HTMLResponse)
 def stake_conference_program(request: Request, meeting: str):
     if meeting == "sunday":
-        program_template = "2024_sunday_morning.html"
+        program_template = "sunday_morning.html"
     elif meeting == "leadership":
-        program_template = "2024_leadership.html"
+        program_template = "leadership.html"
     else:
-        program_template = "2024_saturday_night.html"
+        program_template = "saturday_night.html"
 
     return templates.TemplateResponse(program_template, {"request": request})
 
@@ -35,5 +35,5 @@ def stake_conference_program(request: Request, meeting: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-"""
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+#"""
