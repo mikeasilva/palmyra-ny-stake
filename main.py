47b2880c3a69c0f7ce00f50108c77429a83b070e
Mike Silva <mike.a.silva@gmail.com>
2026-05-17 06:45:27 -0400

    Sunday update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def root(request: Request):
     #program_template = "redirect.html"
     program_template = "general.html"
     program_template = "youth.html"
-    program_template = "adult.html"
+    #program_template = "adult.html"
     #program_template = "leadership.html"
     return templates.TemplateResponse(request=request, name=program_template, context={"request": request})
 
